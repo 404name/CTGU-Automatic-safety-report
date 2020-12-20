d8d95790a2a6427bc0586976a826b7e57860efd1
( •̥́ ˍ •̀ू ) <56631419+404name@users.noreply.github.com>
2020-12-20 17:01:15 +0800

    Update 自动报平安.py
    添加Qmsg酱报平安

--- 自动报平安.py
+++ 自动报平安.py
@@ -4,86 +4,94 @@ from bs4 import BeautifulSoup
 # ****************登录*******************
 
 # 自己的账号密码
-# 0开头的要用字符串
-username = '2019112404'
-password = '285017'
-key = '49bdb9375842537a41ebc635a09229b2'
+# key为推送消息的
+users: ${{secrets.users}}
+# '49bdb9375842537a41ebc635a09229b2'
 logUrl = "http://yiqing.ctgu.edu.cn/wx/index/loginSubmit.do"
+[['2019112404', '285017', '49bdb9375842537a41ebc635a09229b2']]
 
 
-def sentMsg(msg):
+def sentMsg(msg, key):
+    if key == null
     headers = {'Content-Type': 'application/json;charset=utf-8'}
     api_url = "https://qmsg.zendee.cn/send/"+key+"?msg=" + msg
-    return requests.post(api_url, headers=headers, timeout=None).content
+    return requests.post(api_url, headers=headers).content
 
 
-header = {
-    # origin:http://yiqing.ctgu.edu.cn
-    # "Content-Type": "application/json;charset=UTF-8",
-    'Referer': "http://yiqing.ctgu.edu.cn/wx/index/login.do?currSchool=ctgu&CURRENT_YEAR=2019",
-    # 模仿谷歌浏览器的登录
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
-}
+for username, pas, key in users:
+    # start_new_thread(report,(usr,pas,))
+    sentOne(username, password, key)
+    # print(log[-1][-1])
 
-yiqingSession = requests.session()
 
-postData = {
-    "username": username,
-    "password": password
-}
+def sentOne(username, password, key):
+    header = {
+        # origin:http://yiqing.ctgu.edu.cn
+        # "Content-Type": "application/json;charset=UTF-8",
+        'Referer': "http://yiqing.ctgu.edu.cn/wx/index/login.do?currSchool=ctgu&CURRENT_YEAR=2019",
+        # 模仿谷歌浏览器的登录
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
+    }
 
-responseRes = yiqingSession.post(logUrl, data=postData, headers=header)
+    yiqingSession = requests.session()
 
-# *******从提交页面获取 表单信息**********
+    postData = {
+        "username": username,
+        "password": password
+    }
 
-# 构建表单（默认身体健康)
-postData = {
-    "ttoken":  '',
-    "province":  "",
-    "city":      "",
-    "district":  "",
-    "adcode":    "",
-    "longitude": "0",
-    "latitude":  "0",
-    "sfqz": "否",
-    "sfys": "否",
-    "sfzy": "否",
-    "sfgl": "否",
-    "status": "1",
-    "sfgr": "否",
-    "szdz": "",
-    "sjh": "",
-    "lxrxm": '',
-    "lxrsjh": '',
-    "sffr": "否",
-    "sffy": "否",
-    "sfgr": "否",
-    "qzglsj": '',
-    "qzgldd": '',
-    "glyy": '',
-    "mqzz": '',
-    "sffx": "否",
-    "qt": "",
-}
+    responseRes = yiqingSession.post(logUrl, data=postData, headers=header)
 
-getFormurl = "http://yiqing.ctgu.edu.cn/wx/health/toApply.do"
-responseRes = yiqingSession.get(getFormurl, timeout=None)
+    # *******从提交页面获取 表单信息**********
 
-# 获取必要信息填入表单
-soup = BeautifulSoup(responseRes.text, "html.parser")
-getFormlist = soup.find_all('input')[0:15]
+    # 构建表单（默认身体健康)
+    postData = {
+        "ttoken":  '',
+        "province":  "",
+        "city":      "",
+        "district":  "",
+        "adcode":    "",
+        "longitude": "0",
+        "latitude":  "0",
+        "sfqz": "否",
+        "sfys": "否",
+        "sfzy": "否",
+        "sfgl": "否",
+        "status": "1",
+        "sfgr": "否",
+        "szdz": "",
+        "sjh": "",
+        "lxrxm": '',
+        "lxrsjh": '',
+        "sffr": "否",
+        "sffy": "否",
+        "sfgr": "否",
+        "qzglsj": '',
+        "qzgldd": '',
+        "glyy": '',
+        "mqzz": '',
+        "sffx": "否",
+        "qt": "",
+    }
 
-for Formdata in getFormlist:
-    postData[Formdata.attrs['name']] = Formdata.attrs['value']
+    getFormurl = "http://yiqing.ctgu.edu.cn/wx/health/toApply.do"
+    responseRes = yiqingSession.get(getFormurl, timeout=None)
 
-# *************提交最终表单***********
+    # 获取必要信息填入表单
+    soup = BeautifulSoup(responseRes.text, "html.parser")
+    getFormlist = soup.find_all('input')[0:15]
 
-postFormurl = "http://yiqing.ctgu.edu.cn/wx/health/saveApply.do"
+    for Formdata in getFormlist:
+        postData[Formdata.attrs['name']] = Formdata.attrs['value']
 
-header['Referer'] = "http://yiqing.ctgu.edu.cn/wx/health/toApply.do"
+    # *************提交最终表单***********
 
-responseRes = yiqingSession.post(
-    postFormurl, data=postData, headers=header, timeout=None)
+    postFormurl = "http://yiqing.ctgu.edu.cn/wx/health/saveApply.do"
 
-print(responseRes.text)
-sentMsg(responseRes.text)
+    header['Referer'] = "http://yiqing.ctgu.edu.cn/wx/health/toApply.do"
+
+    responseRes = yiqingSession.post(
+        postFormurl, data=postData, headers=header, timeout=None)
+
+    print(responseRes.text)
+    sentMsg(responseRes.text, key)
